2c389ed0ff60243db1f999c1b9bf1a99b95ef2f6
Nico Paulino <nicopaulino8@gmail.com>
2021-01-26 14:45:43 -0600

    Add Routes

--- personal-porfolio/src/app/app-routing.module.ts
+++ personal-porfolio/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './components/pages/about/about.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { ProjectsComponent } from './components/pages/projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: ProjectsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
