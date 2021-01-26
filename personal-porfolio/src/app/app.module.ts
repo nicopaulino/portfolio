f19af366f62b42130766a114bd7c71c2b078a35a
Nico Paulino <nicopaulino8@gmail.com>
2021-01-26 14:45:09 -0600

    Add Declarations

--- personal-porfolio/src/app/app.module.ts
+++ personal-porfolio/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { ProjectsComponent } from './components/pages/projects/projects.component';
+import { HeaderComponent } from './components/layout/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    HomeComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
